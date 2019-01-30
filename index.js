81c4973ca82acd124c2ef2a8bbf79e4242a23697
Mike Upton <uptonm@wit.edu>
2019-01-30 18:10:52 -0500

    Added Swagger API Documentation to Root Route

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 require("dotenv").config();
 
 const app = express();
 
 mongoose.connect(
-  process.env.DB_URI, {
+  process.env.DB_URI,
+  {
     useNewUrlParser: true
   }
 );
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(morgan("dev"));
 app.use(cors());
 
@@ -24,6 +29,8 @@ require("./models/repo-update.model");
 require("./models/repo.model");
 
 require("./routes/repo.routes")(app);
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT, () => {
   console.log(`App listening on ${process.env.PORT}`);
--- routes/repo.routes.js
+++ routes/repo.routes.js
@@ -1,7 +1,5 @@
 const actions = require("../controllers/repo.controller");
 module.exports = app => {
-  app.get("/", actions.test);
-
   app.get("/api/repos", actions.getRepos);
   app.get("/api/repos/:id", actions.getRepo);
   app.post("/api/github/hook", actions.post);
