b10b8b152c9aa2f4e4ccd56f6340dd8c4ec306f2
Michael Upton <uptonm@wit.edu>
2019-02-07 11:29:25 -0500

    Added protected routes with temporary static Bearer token

--- routes/repo.routes.js
+++ routes/repo.routes.js
@@ -1,6 +1,14 @@
 const actions = require("../controllers/repo.controller");
 module.exports = app => {
-  app.get("/api/repos", actions.getRepos);
-  app.get("/api/repos/:id", actions.getRepo);
+  app.get("/api/repos", canAccessRoute, actions.getRepos);
+  app.get("/api/repos/:id", canAccessRoute ,actions.getRepo);
   app.post("/api/github/hook", actions.post);
 };
+
+canAccessRoute = (req, res, next) => {
+  if(req.get("Authorization") === process.env.AUTHORIZATION_TOKEN) {
+    next();
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+}
